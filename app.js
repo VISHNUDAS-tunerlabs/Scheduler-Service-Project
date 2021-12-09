b30e6127e2f3dcb035783c262b3eca669ec1c919
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2021-12-09 14:55:24 +0530

    server created

---
+++ app.js
@@ -0,0 +1,14 @@
+/* This is the application entry point. app server is created here */
+const express=require('express')
+const app=express()
+const port=3000
+
+/*sending data*/
+app.get('/',(req,res)=>{
+    res.send('Welcome')
+})
+
+/*setting the server*/
+app.listen(port,()=>{
+    console.log(` app listening at http://localhost:${port}`)
+})
