71fd6c22993f9f0e6c3a227ef0d976b595a7791c
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2021-12-12 16:25:46 +0530

    error resolved/ need routing

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const routes=require('./routes/appRoutes')
 const app=express()
+const mongoose=require('mongoose')
 const port=3000
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+mongoose.connect("mongodb://localhost:27017/taskSheduler",(err)=>{
+    if(!err){
+        console.log("yea")
+    }
+    else{
+        console.log("badd")
+    }
+})
 
 /*setting middleware connection to routes*/
 app.use('/',routes)
