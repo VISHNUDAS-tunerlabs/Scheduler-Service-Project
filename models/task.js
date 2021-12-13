bed3e19ebd5e2ec7751c170a838ac41e5c19e238
VISHNUDAS-tunerlabse <vishnu@tunerlabs.com>
2021-12-13 09:27:52 +0530

    model added

---
+++ models/task.js
@@ -0,0 +1,16 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+
+const taskSchema=new Schema({
+    taskname:{
+        type:String,
+        required:true
+    },
+    description:{type:String},
+    date:{type:Date},
+    time:{type:String,required:true},
+    repeate:{type:String,required:true},
+    finshed:{type:Boolean}    
+})
+const Task=mongoose.model('Task','taskSchema')
+module.exports=Task;
